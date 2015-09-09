82ca3f1b4f1671e45c62b5c62cbc09b87f35bc48
gfscott <gfscott@users.noreply.github.com>
2015-09-08 20:54:37 -0400

    adding footer

--- footer.php
+++ footer.php
@@ -1,5 +1,15 @@
     <div class="Wrap-footer">
-      <footer>Footer</footer>
+      
+      <footer class="Footer">
+        
+        <?php dynamic_sidebar( 'footer_one' ); ?>
+        
+        <?php dynamic_sidebar( 'footer_two' ); ?>
+        
+        <?php dynamic_sidebar( 'footer_three' ); ?>
+              
+      </footer>
+      
     </div>
     
     <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
