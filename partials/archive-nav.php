5a08de8383ffcf84aaa01b02be4a0deb1c7d25ec
gfscott <gfscott@users.noreply.github.com>
2015-09-14 23:47:08 -0400

    adding archive navigation

--- partials/archive-nav.php
+++ partials/archive-nav.php
@@ -7,8 +7,12 @@
 <?php if ( ! is_single() ) : ?>
 <nav class="Pagination">
   <ul class="Pagination-list">
-    <li class="Pagination-list-item"><?php previous_posts_link("Newer"); ?></li>
-    <li class="Pagination-list-item"><?php next_posts_link("Older"); ?></li>
+    <li class="Pagination-list-item Pagination-list-item--newer">
+      <a href="<?php previous_posts(true); ?>" class="Pagination-link Pagination-link--newer Pagination-link--previous"><span class="Pagination-icon Pagination-icon--newer"></span> Newer</a>
+    </li>
+    <li class="Pagination-list-item Pagination-list-item--older">
+      <a href="<?php next_posts(true); ?>" class="Pagination-link Pagination-link--older Pagination-link--next">Older <span class="Pagination-icon Pagination-icon--older"></span></a>
+    </li>
   </ul>
 </nav>
 <?php endif; ?>
