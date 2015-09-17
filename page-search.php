defac3925d803699d4ccbd3c83a2677a7709e16e
gfscott <gfscott@users.noreply.github.com>
2015-09-16 22:12:37 -0400

    adding search functionality

---
+++ page-search.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<div class="Wrap-page">
+  
+  <div class="Search">
+  
+    <h1 class="Search-hed">Search: <span class="Search-query"><?php echo $_GET["q"]; ?></span></h1>
+      
+      <div class="Search-results">
+
+        <script>
+          (function() {
+            var cx = '016219809144295526682:j77gu7abwwm';
+            var gcse = document.createElement('script');
+            gcse.type = 'text/javascript';
+            gcse.async = true;
+            gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+                '//cse.google.com/cse.js?cx=' + cx;
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(gcse, s);
+          })();
+        </script>
+        <gcse:searchresults-only></gcse:searchresults-only>
+      
+      </div>
+    
+  </div>
+  
+</div>
+
+<?php get_footer(); ?>
