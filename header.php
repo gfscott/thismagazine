e439b83ff045b294786830630d393215be369f27
Graham F. Scott <gfscott@gmail.com>
2015-10-27 08:12:21 -0400

    adding Google analytics tag

--- header.php
+++ header.php
@@ -9,6 +9,15 @@
     
     <link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/css/style-66238dcf9c.min.css">
     
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-3071219-4', 'auto');
+      ga('send', 'pageview');
+    </script>
+        
     <?php get_template_part('partials/ad-header-scripts'); ?>
     
   </head>
