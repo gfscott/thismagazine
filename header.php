05d590f33c72387349c111942619f1ba18beae51
gfscott <gfscott@users.noreply.github.com>
2015-10-31 13:59:01 -0400

    yet another attempt to fix logo for IE

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     
     <title><?php bloginfo("name"); wp_title(" → ", true, "left"); ?></title>
     
-    <link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/css/style-be17ebf9e2.min.css">
+    <link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/css/style-7804afc3d0.min.css">
     
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
