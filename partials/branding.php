784331ff630b724636d2678dead50588fe1b9600
Graham F. Scott <gfscott@gmail.com>
2019-06-02 15:21:56 -0400

    updating to correct logo in masthead

--- partials/branding.php
+++ partials/branding.php
@@ -2,6 +2,10 @@
   <a href="<?php bloginfo('url'); ?>">
     <header class="Branding">
       <h1 class="Branding-sitename"><?php bloginfo('name'); ?></h1>
+      <svg viewBox="0 0 400 119" style="width: 100%">
+        <path fill="#d22"
+          d="M325 119c28 0 46-14 46-37v-1c0-21-16-29-42-35-16-3-20-6-20-11 0-4 4-7 11-7 10 0 22 4 32 11l16-22a71 71 0 0 0-47-15c-27 0-44 15-44 36 0 23 19 30 43 36 15 3 19 6 19 11s-4 8-13 8c-13 0-25-5-36-14l-18 21c14 12 33 19 53 19m-94-2h31V4h-31v113zm-120 0h32V74h40v43h31V4h-31v42h-40V4h-32v113zm-75 0h31V31h34V4H2v27h34v86z" />
+      </svg>
       <p class="Branding-tagline"><?php bloginfo('description'); ?></p>
     </header>
   </a>
